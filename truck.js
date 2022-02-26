f90c792ebb0f95265b9bf21f609a51e1b499c9ae
selenajin99 <selenajin9@gmail.com>
2022-02-26 17:59:16 -0500

    add is_before

--- truck.js
+++ truck.js
@@ -36,14 +36,29 @@ function addResult(inputTripId, loadIds) {
   };
 }
 
-function get_next_trips(origin_latitude, origin_longitude, end_time) {
+function get_next_trips(
+  origin_latitude,
+  origin_longitude,
+  current_time,
+  trucker_end_time
+) {
   // possible trips to be returned
   var possible_trips = [];
+
+  // variables used:
+  // current_time: trucker's time and location when requesting posiible trips
+  // trucker_end_time: the max time trucker can be on the road
+  // travel_to_pickup_time: time in hours it takes to go from trucker's current location to the pickup location
+  // trip_travel_time: time in hours it takes to complete the trip (go form pickup to drop off)
+  // arrival_to_pickup_time: time when the trucker will arrive at pickup location
   for (let i = 0; i < all_trips.length; i++) {
     // check if the trip can be made in time
-    if (is_before(all_trips[i].pickup_date_time, end_time)) {
+    if (
+      is_before(current_time, all_trips[i].pickup_date_time) &&
+      is_before(all_trips[i].pickup_date_time, trucker_end_time)
+    ) {
       // travel time is in hours
-      let travel_time =
+      let travel_to_pickup_time =
         1609.34 * // miles to meters conversion
         55 * // travel speed in miles
         get_distance(
@@ -52,10 +67,25 @@ function get_next_trips(origin_latitude, origin_longitude, end_time) {
           all_trips[i].origin_latitude,
           all_trips[i].origin_longitude
         );
-      let trip_end_time = add_hours_to_time(pickup_date_time, travel_time);
-
+      let trip_travel_time =
+        1609.34 * // miles to meters conversion
+        55 * // travel speed in miles
+        get_distance(
+          all_trips[i].origin_latitude,
+          all_trips[i].origin_longitude,
+          all_trips[i].destination_latitude,
+          all_trips[i].destination_latitude
+        );
+      let trip_end_time = add_hours_to_time(pickup_date_time, trip_travel_time);
+      let arrival_to_pickup_time = add_hours_to_time(
+        current_time,
+        travel_to_pickup_time
+      );
       // check if the trip can end before the trucker's max_destination_time
-      if (is_before(trip_end_time, end_time)) {
+      if (
+        is_before(arrival_to_pickup_time, all_trips[i].pickup_date_time) &&
+        is_before(trip_end_time, end_time)
+      ) {
         // add trip to possible_trips array
         possible_trips += all_trips[i];
       }
@@ -65,8 +95,74 @@ function get_next_trips(origin_latitude, origin_longitude, end_time) {
 // TODO: function to compute the finish time of a starting time and the hours elapsed
 function add_hours_to_time(starting_time, hours) {}
 
-// TODO: returns true if time1 is before or equal to time2
-function is_before(time1, time2) {}
+// returns true if time1 is before or equal to time2
+function is_before(time1, time2) {
+  //two formats
+  //"2022-03-04 13:00:00"
+  //"2022-03-02T19:00:00.000Z"
+  let year1 = parseInt(time1.slice(0, 4));
+  let year2 = parseInt(time2.slice(0, 4));
+  // compare year
+  if (year1 > year2) {
+    return false;
+  } else if (year1 < year2) {
+    return true;
+  } else {
+    // compare month
+    let month1 = parseInt(time1.slice(6, 8));
+    let month2 = parseInt(time2.slice(6, 8));
+    if (month1 > month2) {
+      return false;
+    } else if (month1 < month2) {
+      return true;
+    } else {
+      // compare days
+      let day1 = parseInt(time1.slice(9, 11));
+      let day2 = parseInt(time2.slice(9, 11));
+      if (day1 > day2) {
+        return false;
+      } else if (day1 < day2) {
+        return true;
+      } else {
+        // compare hour
+        let hour1 = parseInt(time1.slice(11, 13));
+        let hour2 = parseInt(time2.slice(11, 13));
+
+        if (hour1 > hour2) {
+          return false;
+        } else if (hour1 > hour2) {
+          return true;
+        } else {
+          // compare minutes
+          let min1 = parseInt(time1.slice(14, 16));
+          let min2 = parseInt(time2.slice(14, 16));
+          if (min1 > min2) {
+            return false;
+          } else if (min1 < min2) {
+            return true;
+          } else {
+            // compare seconds
+            let sec1 = parseInt(time1.slice(17, 19));
+            let sec2 = parseInt(time2.slice(17, 19));
+            if (sec1 > sec2) {
+              return false;
+            } else {
+              return true;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+// function test_is_before() {
+//   console.log(is_before("2022-03-04 13:00:00", "2022-03-04 13:00:00")); //true
+//   console.log(is_before("2022-03-05 13:00:00", "2022-03-04 13:00:00")); //false
+//   console.log(is_before("2022-03-04 13:00:01", "2022-03-04 13:00:00")); //false
+//   console.log(is_before("2022-03-04 13:00:01", "2022-03-02T19:00:00.000Z")); //false
+//   console.log(is_before("2022-03-04 13:00:01", "2022-03-04T19:00:00.000Z")); //false
+//   console.log(is_before("2022-03-04 14:00:01", "2022-03-04T19:00:40.000Z")); //true
+// }
 
 // using provided Geodesic distance function to get the distance between coordinates
 function get_distance(lat1, lon1, lat2, lon2) {
