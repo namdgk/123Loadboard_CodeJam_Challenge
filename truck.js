faeed11683008d8aceebbeb056e8a7a278a30d8b
selenajin99 <selenajin9@gmail.com>
2022-02-26 15:53:57 -0500

    add function for getting next possible trips

--- truck.js
+++ truck.js
@@ -1,8 +1,8 @@
-const jsonData1 = require("./Assets/123Loadboard_CodeJam_2022_dataset.json"); //the huge json file
-const jsonData2 = require("./Assets/123Loadboard_CodeJam_2022_input_sample_s300.json"); //7 samples
+const all_trips = require("./Assets/123Loadboard_CodeJam_2022_dataset.json"); //the huge json file
+const all_truckers = require("./Assets/123Loadboard_CodeJam_2022_input_sample_s300.json"); //7 samples
 
 console.log("-------------------------------------");
-console.log(jsonData1[0]);
+console.log(all_trips[0]);
 console.log("-------------------------------------");
 
 //All distances are in miles
@@ -26,3 +26,51 @@ console.log("-------------------------------------");
 //     },
 //     ...
 // ]
+
+function get_next_trips(origin_latitude, origin_longitude, end_time) {
+  // possible trips to be returned
+  var possible_trips = [];
+  for (let i = 0; i < all_trips.length; i++) {
+    // check if the trip can be made in time
+    if (is_before(all_trips[i].pickup_date_time, end_time)) {
+      // travel time is in hours
+      let travel_time =
+        1609.34 * // miles to meters conversion
+        55 * // travel speed in miles
+        get_distance(
+          origin_latitude,
+          origin_longitude,
+          all_trips[i].origin_latitude,
+          all_trips[i].origin_longitude
+        );
+      let trip_end_time = add_hours_to_time(pickup_date_time, travel_time);
+
+      // check if the trip can end before the trucker's max_destination_time
+      if (is_before(trip_end_time, end_time)) {
+        // add trip to possible_trips array
+        possible_trips += all_trips[i];
+      }
+    }
+  }
+}
+// TODO: function to compute the finish time of a starting time and the hours elapsed
+function add_hours_to_time(starting_time, hours) {}
+
+// TODO: returns true if time1 is before or equal to time2
+function is_before(time1, time2) {}
+
+// using provided Geodesic distance function to get the distance between coordinates
+function get_distance(lat1, lon1, lat2, lon2) {
+  const R = 6371000; // metres
+  const φ1 = (lat1 * Math.PI) / 180; // φ, λ in radians
+  const φ2 = (lat2 * Math.PI) / 180;
+  const Δφ = ((lat2 - lat1) * Math.PI) / 180;
+  const Δλ = ((lon2 - lon1) * Math.PI) / 180;
+
+  const a =
+    Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+    Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  const d = R * c; // in metres
+}
